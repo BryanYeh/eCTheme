1eebacf857517a7f6f122d11c56a8285a649da32
Bryan Y <cs337bryan@gmail.com>
2016-03-24 21:10:20 -0700

    use images instead of image placeholder, multilevel nav

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,34 @@ $(document).ready(function(){
       0: {
         items: 1
       }
-    }
+    },
+    autoplay: true,
+	  stopOnHover: true
   });
 });
+
+(function($){
+	$(document).ready(function(){
+    // window.onresize = function(event){
+    //   if($(window).width() > 767){
+    //     $('ul.dropdown-menu [data-toggle=dropdown]').hover(
+    //       function(){
+    //         $(this).parent().siblings().removeClass('open');
+    //         $(this).parent().toggleClass('open');
+    //       },
+    //       function(){
+    //         $(this).parent().siblings().removeClass('close');
+    //         $(this).parent().removeClass('close');
+    //       }
+    //     );
+    //   }
+    // }
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+		});
+
+	});
+})(jQuery);
